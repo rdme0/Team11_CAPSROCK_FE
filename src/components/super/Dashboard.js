48b49f1a6786d31d56a9f1065d6ea038b287ce12
이예은 <2yeeun@gmail.com>
2025-04-29 21:51:40 +0900

    feat: 대쉬보드 아이콘 및 위치 수정

--- src/components/super/Dashboard.js
+++ src/components/super/Dashboard.js
@@ -8,8 +8,7 @@ const Dashboard = ({ dashboardData, dailyWeather }) => {
   
   return (
       <div className="dashboard">
-        <div><span><img className="cursorIcon" src={"assets/icon/fluent_cursor-24-filled.svg"}></img> </span>{dashboardData.address.level2}</div>
-        <h2>{dashboardData.address.level1}</h2>
+        <h2>{dashboardData.address.level1} {dashboardData.address.level2} </h2>
         <div className="tempNow"><img className="mainIcon" src={`assets/icon/${WeatherNow.weather}.svg`}></img>{dashboardData.temp}°</div>
         <div className="tempMinMax"> 최고 {dashboardData.maxTemp}°  최저 {dashboardData.minTemp}°</div>
       </div>
