f2d80bc7afbfa5e0e84e1fe2edb3415704b1af81
이예은 <2yeeun@gmail.com>
2025-05-19 15:08:48 +0900

    feat: 네비게이션 바 설정

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import { Routes, Route } from 'react-router-dom';
 
 import WeatherPage from "./pages/WeatherPage";
 import ClothingPage from "./pages/ClothingPage";
+import FineDustPage from "./pages/FineDustPage";
+import UltravioletPage from "./pages/UltravioletPage"; 
+
 import HamburgerMenu from "./components/HamburgerMenu/HamburgerMenu"
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 function App() {
 
@@ -20,10 +24,16 @@ function App() {
       <div className="App">
         <HamburgerMenu />
         
+        <main className="page-content">
         <Routes>
           <Route path='/' element={<WeatherPage />} ></Route>
           <Route path='/clothing' element={<ClothingPage />} ></Route>
+          <Route path='/mask' element={<FineDustPage />} ></Route>       {/* <-- /mask 경로에 MaskPage 연결 */}
+          <Route path='/sun' element={<UltravioletPage />} ></Route> 
         </Routes>
+        </main>
+
+        <NavigationBar />
 
       </div>
   );
