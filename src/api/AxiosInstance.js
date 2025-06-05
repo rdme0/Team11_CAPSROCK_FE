b0679f35efde01dfc78dfea0133874e97ca9f8bc
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-06-05 22:04:47 +0900

    deploy: 배포 세팅

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://sbwe6610.iptime.org',
+  baseURL: 'https://sbwe6610.mooo.com',
   timeout: 20000,
   headers: { 'Content-Type': 'application/json' },
 })
